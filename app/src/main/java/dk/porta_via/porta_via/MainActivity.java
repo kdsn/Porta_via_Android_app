d6b6ea0da658b581a75806071a08631b46379dc1
kdsn <ks@get-online.dk>
2020-11-28 09:51:00 +0100

    Initial commit

---
+++ app/src/main/java/dk/porta_via/porta_via/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.porta_via.porta_via;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
